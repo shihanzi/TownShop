961861d4c1bac2c0b5367c14e70b344dc0cbdebc
Shihan Mohamed <shihanmails@gmail.com>
2022-05-18 21:55:26 +0530

    First Commit

---
+++ TownWeb/API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+           var products = await _repo.GetProductsAsync();
+           return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            return await _repo.GetProductsByIdAsync(id);
+        }
+    }
+}
