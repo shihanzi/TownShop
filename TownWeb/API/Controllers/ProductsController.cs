8c53d71ac3c3693740560f885bd5d0cae6a7f158
Shihan Mohamed <shihanmails@gmail.com>
2022-05-20 11:21:29 +0530

    API Products

--- TownWeb/API/Controllers/ProductsController.cs
+++ TownWeb/API/Controllers/ProductsController.cs
@@ -21,13 +21,26 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-           var products = await _repo.GetProductsAsync();
+           var products = await _repo.GetProductAsync();
            return Ok(products);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProducts(int id)
         {
-            return await _repo.GetProductsByIdAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
+        [HttpGet("brands")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok (await _repo.GetProductBrandsAsync());
+        }
+         [HttpGet("types")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypes()
+        {
+            return Ok (await _repo.GetProductTypesAsync());
+        }
+
     }
 }
