b373c03eb1f543063907ad99eb2946751f43ba0f
Shihan Mohamed <shihanmails@gmail.com>
2022-06-18 18:30:01 +0530

    error handling2

--- TownWeb/Core/Specifications/BaseSpecification.cs
+++ TownWeb/Core/Specifications/BaseSpecification.cs
@@ -21,8 +21,31 @@ namespace Core.Specifications
         public List<Expression<Func<T, object>>> Includes {get;} 
         = new List<Expression<Func<T, object>>>();
 
+        public Expression<Func<T, object>> OrderBy {get; private set;}
+
+        public Expression<Func<T, object>> OrderByDescending {get; private set;}
+
+        public int Take {get; private set;}
+
+        public int Skip {get; private set;}
+
+        public bool IsPagingEnabled {get; private set;}
+
         protected void AddInclude(Expression<Func<T, object>> IncludeExression){
             Includes.Add(IncludeExression);
+        }
+        protected void AddOrderBy(Expression<Func<T,object>>OrderByExpression){
+            OrderBy = OrderByExpression;
+        }
+         protected void AddOrderByDescending(Expression<Func<T,object>>OrderByDescendingExpression){
+            OrderByDescending = OrderByDescendingExpression;
+        }
+
+        protected void ApplyPaging (int take,int skip)
+        {
+            Take = take;
+            Skip = skip;
+            IsPagingEnabled = true;
         }
     }
 }
